f1f72a5df06e48daf99707b21c31f497addbab7b
banyaphon <92526947+bypkt-bk@users.noreply.github.com>
2025-01-05 01:54:59 +0700

    Initial commit: Project setup

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), vue(), react(), mdx()]
+});
